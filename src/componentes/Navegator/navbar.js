8919eade1304fb928ac8f4b31c0f48877ff7431c
dmpeprlaza94 <marcelaperlaza-094@hotmail.com>
2020-08-01 00:59:59 -0500

    cree la primera seccion responsive sin el boton

--- src/componentes/Navegator/navbar.js
+++ src/componentes/Navegator/navbar.js
@@ -6,11 +6,17 @@ import logo from "../imagenes/logo.svg";
 const Nav = styled.nav`
   width: 100%;
   height: 80px;
-  padding: 0 20px;
+  padding: 0 5px;
   display: flex;
   justify-content: space-between;
   .logo {
-    padding: 15px 0;
+    padding: 15px 25px;
+  }
+
+  @media (max-width: 768px) {
+    .logo {
+      padding: 15px 15px;
+    }
   }
 `;
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Navegador from "../src/componentes/Navegator/navbar";
-
+import Text from "../src/componentes/SectionFirst/Text";
 ReactDOM.render(
   <React.StrictMode>
     <Navegador />
+    <Text />
   </React.StrictMode>,
   document.getElementById("root")
 );
